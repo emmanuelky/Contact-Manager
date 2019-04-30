921c86ad5f0e123d30fcc2df0399be27ee41c4af
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-04-30 16:32:50 +0200

    contactinfo component

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import ContactInfo from "./ContactInfo";
 
 export default class Contacts extends Component {
   state = {
     contacts: [
       {
         id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
+        name: "John Doe",
+        email: "john@gmail.com",
+        phone: "555-555-5555"
       },
       {
         id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
+        name: "Karen Williams",
+        email: "karen@gmail.com",
+        phone: "444-444-4444"
       },
       {
         id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-333'
+        name: "Henry Johnson",
+        email: "henry@gmail.com",
+        phone: "333-333-333"
       }
     ]
   };
@@ -33,11 +33,9 @@ export default class Contacts extends Component {
           <span className="text-danger">Contact</span> List
         </h1>
         {contacts.map(contact => (
-          <div key={contact.id} contact={contact} />
+          <ContactInfo key={contact.id} contact={contact} />
         ))}
       </React.Fragment>
     );
   }
 }
-
-
